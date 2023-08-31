46b7bf0fa9cb52ee4def5079156ac0fb00f83cc0
R3GAL0 <137714605+R3GAL0@users.noreply.github.com>
2023-08-31 17:10:44 -0400

    added all prompts

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const questions = [
 // project title
 // do you want 'x' section
 // section content if yes
-    // description
-    // table of contents - works with links
-    // installation
-    // usage
-    // license
-    // Contributing
-    // tests
-    // Questions
-    // What licesnse do you want?
+// description
+// table of contents - works with links
+// installation
+// usage
+// license
+// Contributing
+// tests
+// Questions
+// What licesnse do you want?
 
 // add a badge for the seelected license to the readme
 
@@ -40,51 +40,47 @@ function init() {
 init();
 
 
-function askQuestions () {
+function askQuestions() {
     inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: questions[0],
-      },
-      {
-        type: 'checkbox',
-        message: questions[1], // optional sections
-        name: 'sections',
-        choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'License', 'Contributors', 'Tests', 'FAQ'],
-      },
-      {
-        type: 'list',
-        message: questions[3],
-        name: 'license',
-        choices: ['none', 'MIT', 'Unlicense'],
-      },
-      {
-        type: 'input',
-        name: 'githubUser',
-        message: questions[4],
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: questions[5],
-      }
-    ])
-    .then((data) => {
-        console.log(data); // returns below
-        //{
-//   title: 'titel',
-//   sections: [ 'Description', 'Usage', 'License' ],
-//   license: 'none',
-//   githubUser: 'max',
-//   email: '@roger'
-// }
-    //   const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-  
-    // writeToFile(filename, data);git 
-    });
-  
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: questions[0],
+            },
+            {
+                type: 'checkbox',
+                message: questions[1], // optional sections
+                name: 'sections',
+                choices: ['Description', 'Table of Contents', 'Installation', 'Usage', 'License', 'Contributors', 'Tests', 'FAQ'],
+            },
+            {
+                type: 'input',
+                name: 'githubUser',
+                message: questions[4],
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: questions[5],
+            }
+        ])
+        .then((data) => {
+            console.log(data);
+            let { } = data;
+
+            //{
+            //   title: 'titel',
+            //   sections: [ 'Description', 'Usage', 'License' ],
+            //   license: 'none',
+            //   githubUser: 'max',
+            //   email: '@roger'
+            // }
+            //   const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+            // writeToFile(filename, data);git 
+        });
+
 }
 
 // ask questons
@@ -105,13 +101,13 @@ function writeToFile(fileName, data) {
 function formatData(data) {
     // data structure
     //{
-//   title: 'titel',
-//   sections: [ 'Description', 'Usage', 'License' ],
-//   license: 'none',
-//   githubUser: 'max',
-//   email: '@roger'
-// }
-    const {title, sections, license, githubUser, email} = data;
+    //   title: 'titel',
+    //   sections: [ 'Description', 'Usage', 'License' ],
+    //   license: 'none',
+    //   githubUser: 'max',
+    //   email: '@roger'
+    // }
+    const { title, sections, license, githubUser, email } = data;
     /* req variables:
         descTxt
         installTxt
@@ -120,7 +116,7 @@ function formatData(data) {
 
     */
     let markdownTxt = // need to add links for toc?
-`# ${title}
+        `# ${title}
 
 ## Description
 
@@ -148,10 +144,10 @@ ${contribTxt}
 ## License
 
 `
-// add badges to the license
-markdownTxt.concat(pickLicense(license, githubUser, '2023'));
+    // add badges to the license
+    markdownTxt.concat(pickLicense(license, githubUser, '2023'));
 
-`
+    `
 ## Tests
 
 ${testsTxt}
@@ -166,31 +162,119 @@ ${questionsTxt}`
 // need to add badges to the licenses still
 // If there is no license, return an empty string
 function pickLicense(license, details) {
-      // takes an int from 0-2. Returns a license string based on selection.
-  // details is an array: the fullname of the author @ 0 and the current year @ 1
-  
-  if (license == 0) {
-    return '';
-  }
-  if (license == 1) {
+    // takes an int from 0-2. Returns a license string based on selection.
+    // details is an array: the fullname of the author @ 0 and the current year @ 1
+
+    if (license == 0) {
+        return '';
+    }
+    if (license == 1) {
+
+        // MIT license
+        return `##License\n\nMIT License\n\nCopyright (c) [${details}] [2023]\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+    }
+
+    if (license == 2) {
+        // The Unlicense
+        return '##License\n\nThis is free and unencumbered software released into the public domain.\n\nAnyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.\n\nIn jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n\nFor more information, please refer to <https://unlicense.org>';
+    } else {
+        return '';
+    }
+    //  append a license link
+    /*
     
-  // MIT license
-  return `##License\n\nMIT License\n\nCopyright (c) [${details}] [2023]\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+      if (license == 2){
+        return '<https://unlicense.org>';
+      } else {
+        return '';
+      }
+    */
 }
 
-if (license == 2) {
-  // The Unlicense
-  return '##License\n\nThis is free and unencumbered software released into the public domain.\n\nAnyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means.\n\nIn jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n\nFor more information, please refer to <https://unlicense.org>';
-} else {
-  return '';
-}
-//  append a license link
-/*
-
-  if (license == 2){
-    return '<https://unlicense.org>';
-  } else {
-    return '';
-  }
-*/
+// fills all sections except TOC and license
+function fillSections(sections) {
+    
+    // empty array to push section text into specific spots
+    let sectionTxt = ['','' ,'' ,'' ,'' , '','' ]; 
+    // now prompt based on data to fill out selected sections
+    if (sections.includes('description')) {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'What do you want your description section to say?',
+                    name: 'response',
+                }
+            ])
+            .then((r) => {
+                sectionTxt[0] = r.response;
+            });
+    }
+    if (sections.includes('installation')) {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'What do you want your installation section to say?',
+                    name: 'response',
+                }
+            ])
+            .then((r) => {
+                sectionTxt[2] = r.response;
+            });
+    }
+    if (sections.includes('usage')) {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'What do you want your usage section to say?',
+                    name: 'response',
+                }
+            ])
+            .then((r) => {
+                sectionTxt[3] = r.response;
+            });
+    }
+    if (sections.includes('contributing')) {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'What do you want your contributing section to say?',
+                    name: 'response',
+                }
+            ])
+            .then((r) => {
+                sectionTxt[4] = r.response;
+            });
+    }
+    if (sections.includes('tests')) {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'What do you want your tests section to say?',
+                    name: 'response',
+                }
+            ])
+            .then((r) => {
+                sectionTxt[5] = r.response;
+            });
+    }
+    if (sections.includes('questions')) {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'What do you want your questions section to say?',
+                    name: 'response',
+                }
+            ])
+            .then((r) => {
+                sectionTxt[6] = r.response;
+            });
+    }
+    
+    return sectionTxt;
 }
--- read.md
+++ read.md
@@ -5,3 +5,24 @@
 - [Usage](#usage)
 - [Credits](#credits)
 - [License](#license)
+
+
+## Sections
+
+
+description
+installation
+usage
+license
+Contributing
+tests
+Questions
+
+
+table of contents - works with links
+
+
+need to finish: 
+license section
+    badges and links
+
